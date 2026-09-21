fa39e39baf32973128c888eca580380a2b9103cf
Subzar Ahmed <sabzarsaleem690@gmail.com>
2026-09-21 17:50:48 +0530

    Add Day 1 Band Name Generator

---
+++ task.py
@@ -0,0 +1,6 @@
+# project band name generator
+print("Welcome to the brand name generator")
+city = input("what's the name of the city you grew up in?\n")
+pet = input("what's your pet's name?\n")
+print("your band name could be")
+print(city + " " + pet)
