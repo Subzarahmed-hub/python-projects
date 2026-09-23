c4bcfd6805f7d14364d9152b0847169f6d8ad907
Subzar Ahmed <sabzarsaleem690@gmail.com>
2026-09-23 10:36:59 +0530

    Update and rename task.py to Day-2-Tip-Calculator/main.py

--- Day-2-Tip-Calculator/main.py
+++ Day-2-Tip-Calculator/main.py
@@ -9,11 +9,3 @@ bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
 print(f"Each person should pay: ${final_amount}")
 
-# print("welcome to the tip calculator")
-# Bill = float(input("what was the total bill?$"))
-# Tip = int(input("How much tip would you like to give? 10, 12, or 15?$"))
-# people = int(input("How many people to split the bill?$"))
-# bill_with_tip = Tip / 100 * Bill + Bill
-# Each_person_should_pay = (150.00/5)*1.12
-# print(Each_person_should_pay)
-# print(bill_with_tip)
