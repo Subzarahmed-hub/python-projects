0a660d22c67f29d1bb06668d26883fe6d5d64863
Subzar Ahmed <sabzarsaleem690@gmail.com>
2026-09-23 15:42:00 +0530

    Add Day 4 Rock Paper Scissors

---
+++ task.py
@@ -0,0 +1,48 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+print("welcome to the rock paper scissors game")
+choices = ["rock", "paper", "scissors"]
+user_choices = int(input("what would you like to choose, 0 for rock, 1 for paper, 2 for scissors: "))
+print("user choose: ", choices[user_choices])
+print(game_images[user_choices])
+computer_choice = random.randint(0, 2)
+print("Computer choose:", choices[computer_choice])
+print(game_images[computer_choice])
+# print("You choose:", choices[user_choices])
+# print("Computer chose:", choices[computer_choice])
+if user_choices == computer_choice:
+    print("it's a tie")
+elif user_choices == 0 and computer_choice == 2:
+    print("You win!")
+elif user_choices == 1 and computer_choice == 0:
+    print("You win!")
+elif user_choices == 2 and computer_choice == 1:
+    print("You win!")
+else:
+    print("You lose!")
