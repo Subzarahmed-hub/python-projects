17a3f49cef8f11403ef01eea64229432cf805c64
Subzar Ahmed <sabzarsaleem690@gmail.com>
2026-09-23 16:26:39 +0530

    Update main.py

--- Day-4-Rock-Paper-Scissors/main.py
+++ Day-4-Rock-Paper-Scissors/main.py
@@ -25,27 +25,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
 game_images = [rock, paper, scissors]
 print("welcome to the rock paper scissors game")
 choices = ["rock", "paper", "scissors"]
 user_choices = int(input("what would you like to choose, 0 for rock, 1 for paper, 2 for scissors: "))
-if user_choices >= 3 or user_choices < 0:
-    print("You typed an invalid number. You lose!")
-else:
-   print("user choose: ", choices[user_choices])
-   print(game_images[user_choices])
-   computer_choice = random.randint(0, 2)
-   print("Computer choose:", choices[computer_choice])
-   print(game_images[computer_choice])
-   if user_choices == computer_choice:
+print("user choose: ", choices[user_choices])
+print(game_images[user_choices])
+computer_choice = random.randint(0, 2)
+print("Computer choose:", choices[computer_choice])
+print(game_images[computer_choice])
+
+if user_choices == computer_choice:
     print("it's a tie")
-   elif user_choices == 0 and computer_choice == 2:
+elif user_choices == 0 and computer_choice == 2:
     print("You win!")
-   elif user_choices == 1 and computer_choice == 0:
+elif user_choices == 1 and computer_choice == 0:
     print("You win!")
-   elif user_choices == 2 and computer_choice == 1:
+elif user_choices == 2 and computer_choice == 1:
     print("You win!")
-   else:
+else:
     print("You lose!")
+
 
 
